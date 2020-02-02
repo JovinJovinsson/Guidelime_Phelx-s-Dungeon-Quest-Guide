984699cae0a0e9e3e58afa69f0de3e3cd56ec2b5
JovinJovinsson <jovin.svein@gmail.com>
2020-02-02 20:11:47 +0800

    Added SFK
    Updated the TOC to include SFK

---
+++ 22-30 SFK.lua
@@ -0,0 +1,25 @@
+Guidelime.registerGuide([[
+
+[N20-30 Phelx's Shadowfang Keep (BFD) Guide]
+[D Phelx's guide to acquiring all SFK quests. There are 3 total all are exclusive to the Horde.]
+[GA Horde]
+
+[O] Quests can be obtained from [XP16 16] with 2 requiring [XP18 18].
+[O] This guide starts from Undercity.
+Travel to the Apothecarium near the North-Western bridge and speak to Keeper Bel'dugur to accept [QA1013].
+Run or Fly to [F The Sepulcher].
+Get the Flightpath [P The Sepulcher] if you don't already have it.
+Set your hearth to [S The Sepulcher].
+Speak to Dalar Dawnweaver to accept [QA1014].
+Speak to High Executor Hadrec to accept [QA1098].
+**Now enter Shadowfang Keep.**
+Complete [QC1013].
+The Book for [QC1013] can be found in the room with the boss **Fenrus the Devourer** look on the shelves.
+Complete [QC1014].
+Hearth to [H The Sepulcher].
+Turn in [QT1014][QT1098].
+Fly to [F Undercity].
+Turn in [QT1013].
+
+
+]], "Phelx's Dungeon Quest Guide")
