cd65ba483400a574607d5e6eda06e7ea07b68f41
JovinJovinsson <jovin.svein@gmail.com>
2020-02-02 20:13:20 +0800

    Correction for SFK
    Corrected SFK abbreviation in description. Updated version number in TOC.

---
+++ 22-30 Stocks.lua
@@ -0,0 +1,7 @@
+Guidelime.registerGuide([[
+
+[N20-30 Phelx's Stormwind Stockades (Stocks) Guide]
+[D Phelx's guide to acquiring all SFK quests. There are 3 total all are exclusive to the Horde.]
+[GA Alliance]
+
+]], "Phelx's Dungeon Quest Guide")
