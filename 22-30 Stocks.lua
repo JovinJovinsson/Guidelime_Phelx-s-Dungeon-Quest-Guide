02db3ea4fb3efe3782e5f637f310a5b1fea908cd
JovinJovinsson <5206571+JovinJovinsson@users.noreply.github.com>
2023-12-02 16:10:55 +0000

    Added Stockades

--- 22-30 Stocks.lua
+++ 22-30 Stocks.lua
@@ -1,7 +1,46 @@
 Guidelime.registerGuide([[
 
 [N20-30 Phelx's Stormwind Stockades (Stocks) Guide]
-[D Phelx's guide to acquiring all SFK quests. There are 3 total all are exclusive to the Horde.]
+[D Phelx's guide to acquiring all Stockades quests. There are 6 total all are exclusive to the Alliance.]
 [GA Alliance]
+
+[O] Quests can be obtained from [XP22 22] with 1 requiring you to finish [Q373] in the Deadmines first from [XP16 16], see [NX 13-18 VC].
+[O] One quest requires you to do a pre-quest at [XP25 25], see [NX 25 Stocks Side] (recommended you be [XP27 27]).
+[O] This guide begins in Stormwind at the end of handing in [Q373], your Hearth should still be [S Stormwind].
+[O] To make this super easy make sure you have or keep 3 [CI4306,3] as you go through the Stockades.
+Accept [QA389] from Baros Alexston [G49.0, 30.2 Stormwind].
+Run or Fly to [F Lakeshire].
+Find Guard Berton [G26.6,46.8 Redridge Mountains] and accept [QA386].
+Run or Fly to [F Darkshire].
+Find Councilman Millstipe [G72.0,47.8 Duskwood] and accept [QA377].
+Hearth or Fly to [F Stormwind].
+Now go to Old Town and look for Nikova Raskol wandering around, then accept [QA388].
+Make your way over to The Stockades and speed to Warden Thelwater [G41.2,58.0 Stormwind].
+Complete [QT389], and accept the next chain [QA391].
+Also accept [QA387].
+**Now Enter the Stockades**
+Complete all 6 quests [QC377], [QC386], [QC387], [QC388], [QC391], and [QC378] if you have it.
+Remember to keep 3 [CI4306,3] as you go through the Stockades.
+Exit the Stockades.
+Return to Warden Thelwater [G41.2,58.0 Stormwind] and turn in [QT387], [QT391].
+Accept [QA392].
+Return to Baros Alexston [G49.0, 30.2 Stormwind] in the Cathedral and turn in [QT392].
+Accept [QA393].
+Head to Old Town and find Nikova Raskol to turn in [QT388].
+Then go to Master Matthias Shaw [G75.8, 59.8 Stormwind] and turn in [QT393].
+Accept [QA350].
+Head to the Trade District and find Elling Trias [G60, 64.4 Stormwind] and turn in [QT350].
+Accept [QA2745].
+Head over to Stormwind Keep and find Tyrion [G69.2, 14.6 Stormwind] to turn in [QT2745].
+Accept [QA2746], remember those silk cloth? Handy.
+Now, run out of Stormwind to into Elwynn Forest to [G34, 57.2 Elwynn Forest] inside the house to find Clara's Fresh Apples.
+Click the apples to loot [CI8683,2].
+Head back to Tyrion [G69.2, 14.6 Stormwind] and turn in [QT2746] - there is more quest chain, but I won't cover that here.
+Fly to [F Darkshire].
+Councilman Millstipe [G72.0,47.8 Duskwood] and turn in [QT377].
+Fly to [F Lakeshire].
+Find Guard Berton [G26.6,46.8 Redridge Mountains] and turn in [QT386].
+If you have [Q378], fly to [F Menethil Harbor].
+Run to Motley Garmason [G 49.6,18.2 Wetlands] and turn in [QT378].
 
 ]], "Phelx's Dungeon Quest Guide")
---
+++ 25 Stocks Side.lua
@@ -0,0 +1,19 @@
+Guidelime.registerGuide([[
+
+[N25 Phelx's Stormwind Stockades Side Unlock]
+[D Phelx's guide to unlocking 1 quest which requires you to be [XP25 25] but recommend you to be [XP27 27].]
+[GA Alliance]
+
+[O] This guide begins in Stormwind, your Hearth should still be [S Stormwind].
+Fly to [F Ironforge], or take the Deeprun Tram.
+Leave Ironforge and run towards the North East of the map to take the pass to the Wetlands.
+Run through the Wetlands towards Dun Modr [G 49.6,18.2 Wetlands], at the bridge to Arathi Highlands.
+Look for Motley Garmason and accept [QA303].
+Around Dun Modr [G 48.2,16,100 Wetlands], or South East along the mountains [G 62.2,29.8,100 Wetlands] kill all the units you need to for [QC303].
+Run back to Motley Garmason [G 49.6,18.2 Wetlands] and complete [QT303].
+Accept [QA378].
+Run west over towards Menethil Harbor and get [P Menethil Harbor].
+Hearth or Fly to [F Stormwind], or if you have it and have not done Stockades, [F Lakeshire].
+[NX 22-30 Stocks]
+
+]], "Phelx's Dungeon Quest Guide")
