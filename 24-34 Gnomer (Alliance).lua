1f37b84192e20c1ac38548bd5e863aadc8aaeef6
JovinJovinsson <5206571+JovinJovinsson@users.noreply.github.com>
2023-12-03 11:13:16 +0000

    Gnomeregan Added
    Both Horde and Alliance Gnomeregan has been added.

---
+++ 24-34 Gnomer (Alliance).lua
@@ -0,0 +1,44 @@
+Guidelime.registerGuide([[
+
+[N24-34 Phelx's Gnomeregan (Alliance) Guide]
+[D Phelx's guide to acquiring all Gnomeregean quests. This details the shared & Alliance exclusive quests - 10 in total.]
+[GA Alliance]
+
+[O] Quests can be obtained from [XP20 20], although to get all you must be [XP25 25], however it's strongly recommended you either run multiple times or wait until you're closer to [XP30 30].
+[O] Two of the quests are started from item drops inside ([CI9326] and [CI9308]), with a 3rd starting inside as an escort and ending in Booty Bay.
+[O] The two quests inside require 3 Silver each, so be sure to have 6 Silver.
+[O] This guide assumes you begin in Darnassus.
+Head to Warrior's Terrace and accept [QA2925] from Mathiel [G59.2, 45.4 Darnassus].
+Run or fly to [F Stonetalon Peak].
+Head south to find Gaxim Rustfizzle [G59.4, 67.2 Stonetalon Mountains] and accept [QA2931].
+Make your way to Stormwind.
+Head to Cathedral Square inside the cathedral and accept [QA2923] from Brother Sarno [G40.6, 30.8 Stormwind].
+Go to the Dwarven District to accept [QA2928] from Shoni the Shilent [G55.4, 12.6 Stormwind].
+Use the Deeprun Tram or fly to [F Ironforge].
+Go to Tinker Town to turn in [QT2923] at Tinkmaster Overspark [G69.8, 50.2 Ironforge].
+Accept [QA2922].
+Turn in [QT2931] at Master Mechanic Castpipe [G69.8, 48.4 Ironforge].
+Accept [QA2930].
+Accept [QA2929] from High Tinker Mekkatorque [G69, 49 Ironforge].
+Turn in [QT2925] at Klockmort Spannerspan [G68.2, 46.2 Ironforge].
+Accept [QA2924].
+Run to Kharanos and accept [QA2926] from Ozzie Togglevolt [G45.8, 49.2 Dun Morogh].
+Head to the area outside of Gnomeregan in the Train Depot part and complete [QC2926].
+Run back to Ozzie Togglevolt [G45.8, 49.2 Dun Morogh] and turn in [QT2926].
+Accept [QA2962].
+**Now Enter Gnomeregan**
+Keep an eye out for [CI9326] and [CI9308] so you can get [Q2945] and [Q2951], these are completed INSIDE Gnomeregan.
+Make a macro **/target Kernobee** to and press it occasionally to find [Q2904].
+Complete all the remaining quests, then head back to Kharanos.
+Hand in [QT2962] at Ozzie Togglevolt [G45.8, 49.2 Dun Morogh].
+Head to Ironforge and hand in your quests in Tinker Town.
+Turn in [QA2924] with Klockmort Spannerspan [G68.2, 46.2 Ironforge].
+Turn in [QT2929] with High Tinker Mekkatorque [G69, 49 Ironforge].
+Turn in [QT2930] with Master Mechanic Castpipe [G69.8, 48.4 Ironforge].
+Turn in [QT2922] with Tinkmaster Overspark [G69.8, 50.2 Ironforge].
+Make your way via Hearth, Deeprun Tram or Flightt to [F Stormwind].
+Head to the Dwarven District and turn in [QT2928] with Shoni the Shilent [G55.4, 12.6 Stormwind].
+Make your way to Booty Bay, grab the flight path [P Booty Bay].
+Turn in [QT2904] with Scooty [G27.6, 77.4 Stranglethorn Vale].
+
+]], "Phelx's Dungeon Quest Guide")
---
+++ 24-34 Gnomer (Horde).lua
@@ -0,0 +1,30 @@
+Guidelime.registerGuide([[
+
+[N24-34 Phelx's Gnomeregan (Horde) Guide]
+[D Phelx's guide to acquiring all Gnomeregean quests. This details the shared & Horde exclusive quests - 5 in total.]
+[GA Horde]
+
+[O] Quests can be obtained from [XP20 20], although to get all you must be [XP25 25], however it's strongly recommended you either run multiple times or wait until you're closer to [XP30 30].
+[O] Two of the quests are started from item drops inside ([CI9326] and [CI9308]), with a 3rd starting inside as an escort and ending in Booty Bay.
+[O] The two quests inside require 3 Silver each, so be sure to have 6 Silver.
+[O] This guide assumes you begin in and have your Hearth set to [S Orgrimmar].
+Head to The Valley of Honor and speak to Nogg [G75.8, 25.2 Orgrimmar].
+Accept [QA2841], this unlocks another.
+Accept [QA2842].
+Make your way to Booty Bay, easiest method is fly to [F Ratchet].
+Take the boat to Booy Bay, grab the flight path [P Booty Bay].
+Speak to Scooty [G27.6, 77.4 Stranglethorn Vale] to turn in [QT2842].
+Accept [QA2843], then wait a moment.
+Turn in [QT2843].
+You have unlocked the teleporter to Gnomeregan!
+**Now Enter Gnomeregan**
+Keep an eye out for [CI9326] and [CI9308] so you can get [Q2945] and [Q2951], these are completed INSIDE Gnomeregan.
+Make a macro **/target Kernobee** to and press it occasionally to find [Q2904].
+Complete all the remaining quests.
+Hearth to [H Orgrimmar].
+Head to Nogg [G75.8, 25.2 Orgrimmar] and turn in [QT2841].
+Fly to [F Ratchet].
+Take the boat to Booty Bay.
+Turn in [QT2904] with Scooty [G27.6, 77.4 Stranglethorn Vale].
+
+]], "Phelx's Dungeon Quest Guide")
