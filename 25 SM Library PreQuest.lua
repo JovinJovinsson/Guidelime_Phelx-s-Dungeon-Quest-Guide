710a365a359d5a206dbe5ed3b48d6c7c5b021036
JovinJovinsson <5206571+JovinJovinsson@users.noreply.github.com>
2023-12-06 21:20:18 +0000

    SM Library Horde Complete

--- 25 SM Library PreQuest.lua
+++ 25 SM Library PreQuest.lua
@@ -32,6 +32,9 @@ Accept [QA6627].
 Select Option 2 "Neltharion is my answer".
 Speak to [TAR4489] again and turn in [QT6627].
 Accept [QA1159].
+[GA Orc,Tauren,Troll] Run back to to the flight master in [G 45.2,59.8 Stonetalon Mountains].
+[GA Orc,Tauren,Troll] Fly to [F Thunder Bluff].
+[GA Orc,Tauren,Troll] Go to [G 34.6,47.2 Thunder Bluff] and speak to [TAR3978] to accept [QA1049].
 Make your way to [F Undercity].
 Head to [G 57.8,65 Undercity] and speak to [TAR4488] to turn in [QT1159].
 Accept [QA1160].
