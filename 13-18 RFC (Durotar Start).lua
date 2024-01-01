dd955f09597c58b77379bfba6f4dd418da6116a8
JovinJovinsson <5206571+JovinJovinsson@users.noreply.github.com>
2024-01-01 17:06:54 +0000

    RFC Overhaul
    Updated the 3 RFC guides (based on starting zone). Split off the completion (after zoning in) into its own guide so if people are already in there, they can jump to it, and also reduce rework in writing.

--- 13-18 RFC (Durotar Start).lua
+++ 13-18 RFC (Durotar Start).lua
@@ -10,10 +10,11 @@ Accept [QA5761] from [TAR3216].
 Accept [QA5726] from [TAR4949].
 Complete [QC5726].
 Catch the [G50.8, 13.6 Durotar] Zeppelin to Undercity.
-Get the Flightpath [P Undercity], this is for convenience later on.
+Get the Flightpath [P Undercity].
 Accept [QA5725] from [TAR2425].
 Hearth to [H Orgrimmar].
 Fly to [F Thunderbluff], or run and do the below if you don't have the flightpath.
+[OC] Get the Flightpath [P Ratchet].
 [OC] Get the Flightpath [P Crossroads].
 [OC] Get the Flightpath [P Camp Taurajo].
 [OC] Get the Flightpath [P Thunderbluff].
@@ -24,19 +25,6 @@ Accept [QA5727] from [TAR4949].
 Complete [QC5727] by speaking to [TAR3216].
 Turn in [QT5727] with [TAR4949].
 Accept [QA5728] from [TAR4949].
-**Now enter Ragefire Chasm.**
-Complete [QC5722] at [TAR11834].
-Accept [QA5724] from [TAR11834].
-[O] Complete [QC5728][QC5761][QC5723][QC5725].
-Turn in [QT5761] at [TAR3216].
-Turn in [QT5828] at [TAR4949]
-Accept [QA5729] from [TAR4949].
-Turn in [QT5729] at [TAR3216].
-Accept [QA5730] from [TAR3216].
-Turn in [QT5730] at [TAR4949].
-Catch the [G50.8, 13.6 Durotar] Zeppelin to Undercity.
-Turn in [QT5725] at [TAR2425].
-Hearth to [H Orgrimmar].
-Fly to [F Thunderbluff].
-Turn in [QT5723][QT5724] at [TAR11833].
+**Now enter Ragefire Chasm**
+[NX 13-18 RFC Finisher]
 ]], "Phelx's Dungeon Quest Guide")
---
+++ 13-18 RFC Finisher.lua
@@ -0,0 +1,25 @@
+Guidelime.registerGuide([[
+
+[N13-18 Ragefire Chasm (RFC) Finisher]
+[D This guide walks you through completing & handing in the quests. Check out either the Durotar, Mulgore or Trisifal Glades start guides for the full list of quests.]
+[GA Horde]
+
+[NX 13-18 RFC (Mulgore Start)]
+[NX 13-18 RFC (Durotar Start)]
+[NX 13-18 RFC (Trisifal Glades Start)]
+**Now enter Ragefire Chasm**
+Complete [QC5722] at [TAR11834].
+Accept [QA5724] from [TAR11834].
+[O] Complete [QC5728][QC5761][QC5723][QC5725].
+Turn in [QT5761] at [TAR3216].
+Turn in [QT5828] at [TAR4949]
+Accept [QA5729] from [TAR4949].
+Turn in [QT5729] at [TAR3216].
+Accept [QA5730] from [TAR3216].
+Turn in [QT5730] at [TAR4949].
+Catch the [G50.8, 13.6 Durotar] Zeppelin to Undercity.
+Turn in [QT5725] at [TAR2425].
+Hearth to [H Orgrimmar].
+Fly to [F Thunderbluff].
+Turn in [QT5723][QT5724] at [TAR11833].
+]], "Phelx's Dungeon Quest Guide")
